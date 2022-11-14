028c057fd59c9f53ae9dfbc0d6d1afb3bfdc8c1a
James Anderson <james.anderson2@btinternet.com>
2022-11-14 22:16:36 +0000

    Start implementing message receive

--- Esp32DroneCan/include/Esp32DroneCan.h
+++ Esp32DroneCan/include/Esp32DroneCan.h
@@ -1,14 +1,23 @@
 #pragma once
 #include "BroadcastTransfer.h"
-#include "driver/gpio.h"
-#include "driver/twai.h"
+#include <driver/gpio.h>
+#include <driver/twai.h>
+#include <soc/periph_defs.h>
 class Esp32DroneCan
 {
 public:
-    Esp32DroneCan(uint8_t);
+    Esp32DroneCan(uint8_t, gpio_num_t, gpio_num_t);
     bool broadcast(BroadcastTransfer);
+    void onReceive(void (*callback)(twai_message_t));
 
 private:
     uint8_t _nodeId;
+    gpio_num_t _txPin;
+    gpio_num_t _rxPin;
+    intr_handle_t _interuptHandle;
     bool sendCanMessage(uint32_t, uint8_t *, uint16_t);
+    twai_message_t receiveCanMessage();
+    static void onInterrupt(void *arg);
+    void handleInterrupt();
+    void (*_onReceive)(twai_message_t);
 };
--- Esp32DroneCan/src/Esp32DroneCan.cpp
+++ Esp32DroneCan/src/Esp32DroneCan.cpp
@@ -1,9 +1,16 @@
 #include "Esp32DroneCan.h"
 
-Esp32DroneCan::Esp32DroneCan(uint8_t nodeId) : _nodeId(nodeId)
+Esp32DroneCan::Esp32DroneCan(uint8_t nodeId, gpio_num_t txPin, gpio_num_t rxPin)
+    : _nodeId(nodeId),
+      _txPin(txPin),
+      _rxPin(rxPin),
+      _interuptHandle(NULL)
 {
-    twai_general_config_t gConfig = TWAI_GENERAL_CONFIG_DEFAULT(GPIO_NUM_6, GPIO_NUM_7, TWAI_MODE_NORMAL);
-    twai_timing_config_t tConfig = TWAI_TIMING_CONFIG_500KBITS();
+    twai_general_config_t gConfig = TWAI_GENERAL_CONFIG_DEFAULT(
+        _txPin,
+        _rxPin,
+        TWAI_MODE_NORMAL);
+    twai_timing_config_t tConfig = TWAI_TIMING_CONFIG_1MBITS();
     twai_filter_config_t fConfig = TWAI_FILTER_CONFIG_ACCEPT_ALL();
 
     if (twai_driver_install(&gConfig, &tConfig, &fConfig) != ESP_OK)
@@ -17,22 +24,34 @@ Esp32DroneCan::Esp32DroneCan(uint8_t nodeId) : _nodeId(nodeId)
 
 bool Esp32DroneCan::broadcast(BroadcastTransfer broadcastTransfer)
 {
-    if (broadcastTransfer.getPayload() == NULL || broadcastTransfer.getPayloadLength() > 7)
+    if (broadcastTransfer.getPayload() == NULL ||
+        broadcastTransfer.getPayloadLength() > 7)
         return false;
 
     if (broadcastTransfer.getPriority() > 31)
         return false;
 
-    uint32_t canId = ((uint32_t)broadcastTransfer.getPriority() << 24) | ((uint32_t)broadcastTransfer.getDataTypeId() << 8) | (uint32_t)_nodeId;
+    uint32_t canId = ((uint32_t)broadcastTransfer.getPriority() << 24) |
+                     ((uint32_t)broadcastTransfer.getDataTypeId() << 8) |
+                     (uint32_t)_nodeId;
 
     uint8_t payloadWithTailByte[8];
-    memcpy(payloadWithTailByte, broadcastTransfer.getPayload(), broadcastTransfer.getPayloadLength());
-    payloadWithTailByte[broadcastTransfer.getPayloadLength()] = 0xC0 | (broadcastTransfer.getTransferId() & 31);
+    memcpy(payloadWithTailByte,
+           broadcastTransfer.getPayload(),
+           broadcastTransfer.getPayloadLength());
+    payloadWithTailByte[broadcastTransfer.getPayloadLength()] =
+        0xC0 | (broadcastTransfer.getTransferId() & 31);
 
-    return this->sendCanMessage(canId, payloadWithTailByte, broadcastTransfer.getPayloadLength() + 1);
+    return this->sendCanMessage(
+        canId,
+        payloadWithTailByte,
+        broadcastTransfer.getPayloadLength() + 1);
 }
 
-bool Esp32DroneCan::sendCanMessage(uint32_t canId, uint8_t *payload, uint16_t payloadLength)
+bool Esp32DroneCan::sendCanMessage(
+    uint32_t canId,
+    uint8_t *payload,
+    uint16_t payloadLength)
 {
     twai_message_t message;
     message.identifier = canId;
@@ -41,4 +60,42 @@ bool Esp32DroneCan::sendCanMessage(uint32_t canId, uint8_t *payload, uint16_t pa
     memcpy(message.data, payload, payloadLength);
 
     return twai_transmit(&message, pdMS_TO_TICKS(1000)) == ESP_OK;
+}
+
+void Esp32DroneCan::onReceive(void (*callback)(twai_message_t))
+{
+    _onReceive = callback;
+
+    if (_interuptHandle)
+    {
+        esp_intr_free(_interuptHandle);
+        _interuptHandle = NULL;
+    }
+
+    if (callback)
+    {
+        esp_intr_alloc(
+            ETS_TWAI_INTR_SOURCE,
+            0,
+            Esp32DroneCan::onInterrupt,
+            this,
+            &_interuptHandle);
+    }
+}
+
+void Esp32DroneCan::onInterrupt(void *arg)
+{
+    ((Esp32DroneCan *)arg)->handleInterrupt();
+}
+
+void Esp32DroneCan::handleInterrupt()
+{
+    _onReceive(receiveCanMessage());
+}
+
+twai_message_t Esp32DroneCan::receiveCanMessage()
+{
+    twai_message_t message;
+    twai_receive(&message, pdMS_TO_TICKS(10000));
+    return message;
 }
